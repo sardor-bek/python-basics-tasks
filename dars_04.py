e5f7ea1e70cde5c5cbad7fe63a2e59328e4cfd5a
sardorbek <78154377+sardor-bek@users.noreply.github.com>
2021-06-01 10:31:36 +0500

    Add files via upload

---
+++ dars_04.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 15 16:32:39 2021
+
+@author: 1
+"""
+# O'ZGARUVCHILAR (Variables) 
+# 4-dars
+
+#ism="Abdulloh"
+#yosh=25
+#print(ism, yosh)
+
+#ism="Abdulloh"
+#print(ism)
+#ism="Muhammad"
+#print(ism)
+
+#ism="Sardor"
+#familya="Nazarqulov"
+#yosh=24
+#davlat="Uzbekistan"
+#malumoti="Bakalavr"
+#print(ism, familya, yosh, davlat, malumoti)
+
+# uy ishi
+
+#radius = 5
+#pi = 3.14159
+#aylana_yuzi = pi * radius**2
+#print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
+
+ 
