2e9dd504943e86c75fce33eb8b00e54c52382b47
sardorbek <78154377+sardor-bek@users.noreply.github.com>
2021-06-01 10:30:27 +0500

    Add files via upload

---
+++ dars_03.py.py
@@ -0,0 +1,25 @@
+# 03 dars PRINT() Funksiyasi
+# sana: 13-mart 2021
+#print("Hello World")
+#print("assalomu alaykum")
+#print("hayrli tong!")
+#print('men kecha \"DELL\" markasidagi laptop sotib oldim')
+#print("""odamzotga alq-idrok berilgandir, \nlekin u togri ishlata olmaydi""")
+#print(2**3)
+#print("To'qqizning kvadrati", 9**2, "ga teng")
+
+#Uy ishi 
+#"Nexia", "Tico", 'Damas' ko'rganlar qilar havas matnini konsolga chiqarish
+#print("\"Nexia\", \"Tico\", 'Damas',"ko'rganlar qilar havas")
+
+# 5ning 4-darajasini toping
+#print("5ning 4-darajasi", 5**4, "ga teng")
+
+# 22ni 4ga bo'lganda qancha qoldiq qoladi?
+#print("22ni 4ga bo'lganda ", 22%4, "qoldiq qoladi")
+
+# Tomonlari 125ga teng kvadratning yuzi perimetrini toping
+#print("Tomonlari 125ga teng bo'lgan kvadratning yuzi", 125**2, "ga teng", "perimetri", 4*125, "ga teng")
+
+# Diametri 12ga teng bo'lgan doiraning yuzini toping (π = 3.14 ga teng )
+print("Diametri 12ga teng bo'lgan doiraning yuzi", 3.14*(12/2)**2, "ga teng")
