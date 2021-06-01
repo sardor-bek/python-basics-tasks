91acf843e780f37b6787d295c10c9a7a7d2f0c3a
sardorbek <78154377+sardor-bek@users.noreply.github.com>
2021-06-01 20:18:45 +0500

    Add files via upload

---
+++ dars_05.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 15 18:45:18 2021
+
+@author: 1
+"""
+# 5-dars
+# String (Matn)
+
+#ism ="Sardor"
+
+#joy = "Toshkent" 
+
+#matn = " Men yangi ☏ oldim"
+#print(matn, ism)
+#print("mening ismim " + ism)
+
+# f-string
+
+#ism="Ahad"
+#familya="Qayum"
+#ism_sharif=f"{ism} {familya}"
+#print(ism_sharif)
+
+#ism="James"
+#familya="Bond"
+#print(f"salom mening ismim {familya}. {ism} {familya}")
+
+# STRING METODLAR
+
+#ism='james'
+#familya='bond'
+#ism_sharif=f"{ism} {familya}"
+#ism_sharif = ism_sharif.upper()
+#print(ism_sharif.lower())
+#print(ism_sharif.title())
+#print(ism_sharif.capitalize())
+
+
+
+#ism= input("ismingiz nima?")
+#print("Assalomu alaykum, " + ism)
+
+#ism = input("Ismingiz nima?\t>>>")
+#print("Assalomu alaykum, " + ism.title())
+
+# AMALIYOT
+
+#1
+
+# kocha = "sag'bon"
+# mahalla = "bodomzor"
+# tuman = "mirzo ulug'bek"
+# viloyat = "toshkent"
+# print(kocha +' ' + "ko'chasi" + ' ' + mahalla + ' ' "mahallasi" + ' ' + tuman + ' ' "tumani" + ' ' + viloyat + ' ' + "viloyati")
+
+#2
+
+# kocha = input("qaysi ko'chada yashaysiz?\n>>>") 
+# mahalla = input("qaysi mahallada yashaysiz?\n>>>") 
+# tuman = input("qaysi tumanda yashaysiz?\n>>>") 
+# viloyat = input("qaysi viloyatda yashaysiz?\n>>>") 
+# print("Men, " + kocha.title() + ' ' + "ko'chasi" + ' ' + mahalla.upper() + ' ' + "mahallasi" + ' '  + tuman.lower() + ' ' + "tumani" + ' ' +  viloyat.capitalize() + ' ' + "viloyatda yashayman")
